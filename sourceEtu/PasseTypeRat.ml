0bf7f1a41d8eba2c78def44dc96c60461b277912
Adrien Chevallereau <93998883+AdrienN7@users.noreply.github.com>
2021-12-04 22:00:10 +0100

    passage des test de type sans les fonctions

--- sourceEtu/PasseTypeRat.ml
+++ sourceEtu/PasseTypeRat.ml
@@ -68,9 +68,9 @@ let rec analyse_type_expression e = (* failwith "todo"*)
   | AstTds.Entier n -> (Entier n, Int)
   | AstTds.Unaire (u, e1) -> let (ne1, te1) = analyse_type_expression e1 in
                               begin match u with
-                              | AstSyntax.Denominateur -> if (est_compatible te1 Rat) then Unaire (Denominateur, ne1), te1
+                              | AstSyntax.Denominateur -> if (est_compatible te1 Rat) then Unaire (Denominateur, ne1), Int
                                                           else raise (TypeInattendu (te1, Rat))
-                              | AstSyntax.Numerateur -> if (est_compatible te1 Rat) then Unaire (Numerateur, ne1), te1
+                              | AstSyntax.Numerateur -> if (est_compatible te1 Rat) then Unaire (Numerateur, ne1), Int
                                                         else raise (TypeInattendu (te1, Rat))
                               end
   | AstTds.Binaire (b, e1, e2) -> begin match b, analyse_type_expression e1, analyse_type_expression e2 with
@@ -137,6 +137,7 @@ let rec analyse_type_instruction tf i =
       | Some t -> if (t=te) then Retour ne
                   else raise (TypeInattendu (t, te))
       end
+  | AstTds.Empty -> Empty
       
 (* analyse_tds_bloc : AstSyntax.bloc -> AstTds.bloc *)
 (* Paramètre tds : la table des symboles courante *)
